1cd2a98ad11b772b55e1713f6f44692b5ee42eac
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-04 21:15:54 +0100

    data age act

---
+++ basics/input/data_types.py
@@ -0,0 +1,20 @@
+print("what is your name?")
+name = input()
+
+print("what is your   age?")
+age = int(input())
+
+print("what is your weight?(ponund)")
+
+weight = float(input())
+
+print("what is your hight?")
+height = float(input())
+
+bmi = weight / (height ** 2)
+print(f"{name} your bmi is {bmi}")
+
+
+
+
+
