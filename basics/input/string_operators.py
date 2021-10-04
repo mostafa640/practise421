e93626572030e38f368df37cc31fbcea7dc2c655
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-04 21:47:05 +0100

    string 4

---
+++ basics/input/string_operators.py
@@ -0,0 +1,13 @@
+print("Please enter number of lives")
+lives = int(input())
+
+print("Please enter energy level")
+energy = int(input())
+
+print("Please enter shield level")
+shield = int(input())
+
+print(f"Lives:  {'♥' * lives}")
+print(f"Energy:  {'♦' * energy}")
+print(f"Shield:  {'♦' * shield}")
+
