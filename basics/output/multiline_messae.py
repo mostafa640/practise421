6cb40bd4ce2274ac752c8491c6413b0a3a2c09e1
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-04 17:22:44 +0100

    i am beep

--- basics/output/multiline_messae.py
+++ basics/output/multiline_messae.py
@@ -1,2 +1,7 @@
 print("system message implement")
 print("system robot has been initiated")
+print("\n displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\ Displays a single quote")
